7494db34f590e122c71d432488e006136bdbe69a
John Haugeland <stonecypher@gmail.com>
2015-07-18 10:35:18 -0700

    p2 - find the last-but-one element of a list

--- src/p99l.erl
+++ src/p99l.erl
@@ -9,7 +9,8 @@
 
 -export([
 
-    p1/1
+    p1/1,
+    p2/1
 
 ]).
 
@@ -18,7 +19,7 @@
 
 
 % find the last element of a list
-
 p1(List) -> lists:last(List).
 
-
+% find the last-but-one element of a list
+p2(List) -> lists:nth(length(List) - 1, List).
