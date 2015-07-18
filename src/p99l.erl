7d67c4211ba22932e1747bb4b5c2e28a02643e44
John Haugeland <stonecypher@gmail.com>
2015-07-18 10:33:38 -0700

    p1 - find the last element of a list

---
+++ src/p99l.erl
@@ -0,0 +1,24 @@
+
+-module(p99l).
+
+% speed run.  shameless answers if correct.
+
+
+
+
+
+-export([
+
+    p1/1
+
+]).
+
+
+
+
+
+% find the last element of a list
+
+p1(List) -> lists:last(List).
+
+
