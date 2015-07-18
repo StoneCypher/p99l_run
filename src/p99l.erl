2ba9a4981cd5006b67412ed3949ccf5186a09317
John Haugeland <stonecypher@gmail.com>
2015-07-18 10:36:29 -0700

    p3 - find the kth element of a list

--- src/p99l.erl
+++ src/p99l.erl
@@ -7,19 +7,17 @@
 
 
 
--export([
-
-    p1/1,
-    p2/1
-
-]).
+-compile(export_all).
 
 
 
 
 
 % find the last element of a list
-p1(List) -> lists:last(List).
+p1(List)    -> lists:last(List).
 
 % find the last-but-one element of a list
-p2(List) -> lists:nth(length(List) - 1, List).
+p2(List)    -> lists:nth(length(List) - 1, List).
+
+% find the kth element of a list
+p3(K, List) -> lists:nth(K, List).
