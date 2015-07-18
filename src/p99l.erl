94b8cb1789d8821c9164c0f4e3d7017c80b1a027
John Haugeland <stonecypher@gmail.com>
2015-07-18 10:46:51 -0700

    p8 - remove repetition

--- src/p99l.erl
+++ src/p99l.erl
@@ -33,3 +33,15 @@ p6(List)    -> lists:reverse(List) == List.
 
 % flatten
 p7(DList)   -> lists:flatten(DList).
+
+
+
+
+
+% eliminate consecutive duplicates
+p8([])              -> [];
+p8([ First | Rem ]) -> p8_work(First, Rem, []).
+
+p8_work(Current, [],              Extracted) -> lists:reverse([Current] ++ Extracted);
+p8_work(Current, [Current | Rem], Extracted) -> p8_work(Current, Rem, Extracted);
+p8_work(Current, [New     | Rem], Extracted) -> p8_work(New,     Rem, [Current] ++ Extracted).
